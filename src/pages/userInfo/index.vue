3accb387754e7c003c099848ddad39bbb0b02217
zhongpeng <859247268@qq.com>
2021-07-20 12:26:21 +0800

    feat: 用户信息页

--- src/pages/userInfo/index.vue
+++ src/pages/userInfo/index.vue
@@ -44,19 +44,171 @@
         </view>
       </view>
     </view>
-    <view class="listWrapper"></view>
+    <cl-tabs
+      color="#00c58e"
+      class="tabs"
+      v-model="current"
+      :labels="labels"
+      :border="false"
+    >
+      <!-- 自定义内容区域 -->
+      <swiper class="container" @change="onChangeSwiper" :current="current">
+        <swiper-item v-for="(item, index) in list" :key="index">
+          <!-- 是否预加载 -->
+          <template v-if="item.loaded || index == current">
+            <scroll-view
+              class="scroll-view-wrapper"
+              :scroll-y="true"
+              :refresher-enabled="isRefresh"
+              refresher-default-style="black"
+              :upper-threshold="150"
+              :lower-threshold="150"
+              @scrolltoupper="onDown"
+              @scrolltolower="onUp"
+            >
+              <cl-loading-mask :loading="loading" text="加载中">
+                <view
+                  v-for="(item2, index2) in item.data"
+                  :key="index2"
+                  class="scroll-view-item"
+                >
+                  <ListItem />
+                </view>
+
+                <cl-loadmore
+                  v-if="item.data.length > 0"
+                  :loading="item.loading"
+                  :finish="item.finished"
+                  :divider="false"
+                ></cl-loadmore>
+              </cl-loading-mask>
+            </scroll-view>
+          </template>
+        </swiper-item>
+      </swiper>
+    </cl-tabs>
   </view>
 </template>
 
 <script>
+import ListItem from "./components/ListItem.vue";
+
 export default {
   data() {
-    return {};
+    const labels = [
+      {
+        label: "动态",
+        value: 1,
+        loaded: true,
+      },
+      {
+        label: "文章",
+        value: 2,
+      },
+      {
+        label: "互动",
+        value: 3,
+      },
+      {
+        label: "其他",
+        value: 4,
+      },
+    ];
+
+    const list = labels.map((e) => {
+      return {
+        ...e,
+        status: e.value,
+        data: [],
+        finished: false,
+        loading: false,
+        pagination: {
+          page: 1,
+          size: 20,
+        },
+      };
+    });
+
+    return {
+      current: 0,
+      labels,
+      list,
+      loading: true,
+      isRefresh: true, // 是否开启下拉刷新
+    };
   },
-  onLoad() {},
+  components: {
+    ListItem,
+  },
+  onLoad() {
+    this.refresh();
+  },
   methods: {
     goback() {
       uni.navigateBack();
+    },
+    onDown() {
+      console.log("====>");
+      console.log("down");
+      this.refresh({
+        page: 1,
+      }).done(() => {
+        this.$refs[`scroller-${this.current}`][0].end();
+      });
+    },
+
+    onUp() {
+      console.log("====>");
+      console.log("up");
+      const { pagination, finished } = this.list[this.current];
+
+      if (!finished) {
+        this.refresh({
+          page: pagination.page + 1,
+        });
+      }
+    },
+
+    onChangeSwiper(e) {
+      this.current = e.detail.current;
+
+      if (!this.list[this.current].loaded) {
+        this.loading = true;
+        this.list[this.current].loaded = true;
+      }
+
+      setTimeout(() => {
+        this.refresh({
+          page: 1,
+        });
+      }, 500);
+    },
+
+    refresh(params = {}) {
+      const item = this.list[this.current];
+
+      let data = {
+        ...item.pagination,
+        status: item.status,
+        sort: "desc",
+        order: "createTime",
+        ...params,
+      };
+
+      return new Promise((resolve) => {
+        item.loading = true;
+
+        console.log("Refresh");
+
+        setTimeout(() => {
+          item.data = new Array(data.page == 1 ? 12 : data.page * 12).fill(1);
+          item.pagination.page = data.page;
+          item.finished = false;
+          item.loading = false;
+          this.loading = false;
+          resolve();
+        }, 500);
+      });
     },
   },
 };
@@ -69,7 +221,7 @@ export default {
   position: relative;
 
   .bgImg {
-    height: 200rpx;
+    height: 300rpx;
     background-repeat: no-repeat;
     background-position: center 6%;
     background-size: cover;
@@ -94,7 +246,7 @@ export default {
 
   .avatar {
     position: absolute;
-    top: 112rpx;
+    top: 216rpx;
     left: 28rpx;
     border: 4rpx solid #fff;
     @include setBgImg(160rpx, 160rpx, 50%);
@@ -137,6 +289,19 @@ export default {
         .word {
           @include setSmallTitle(#aeb4bb, 48rpx, 28rpx);
         }
+      }
+    }
+  }
+
+  .tabs {
+    margin-bottom: 16rpx;
+
+    .container {
+      height: 740rpx;
+      background-color: #f7f7f7;
+
+      .scroll-view-wrapper {
+        height: 100%;
       }
     }
   }
